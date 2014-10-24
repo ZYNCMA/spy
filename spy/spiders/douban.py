b8cd65bf93ebfa1365c5fbc936f162dd68f5120d
zync <zync.mzy@gmail.com>
2014-10-24 19:48:17 +0800

    almost there

--- spy/spiders/douban.py
+++ spy/spiders/douban.py
@@ -6,6 +6,12 @@ import os
 import sys
 import MySQLdb
 
+def get_id(url):
+    return url.split('/')[-2]
+
+def get_page(url):
+    return url.split('=')[-1]
+
 class douban(BaseSpider):
     reload(sys)
     sys.setdefaultencoding('utf8')
@@ -24,11 +30,18 @@ class douban(BaseSpider):
 
     start_urls = []
     info = {}
-    cur.execute('select url from douban')
-    urls = cur.fetchall()
-    for url in urls:
-        url = url[0].strip()+"?start=0"
+    cur.execute('select uid, last, `update`, update_last, `total`, total_last, author from douban')
+    rsts = cur.fetchall()
+    for rst in rsts:
+        url = "http://www.douban.com/group/topic/"+rst[0].strip()+"/?start="+str(rst[1])
         start_urls.append(url)
+
+        uid = get_id(url)
+        info[uid] = {"total": rst[4]-rst[5]}
+        info[uid]["total_last"] = 0
+        info[uid]["update"] = rst[2]-rst[3]
+        info[uid]["update_last"] = 0
+        info[uid]["author"] = rst[6]
 
     def __init__(self, url=None, *args, **kwargs):
         if url:
@@ -36,19 +49,34 @@ class douban(BaseSpider):
         super(douban, self).__init__(*args, **kwargs)
 
     def handle_last(self, response):
-        return []
+        uid = get_id(response.url)
+        page = get_page(response.url)
+        hxs = Selector(response)
+
+        if page == "0":
+            self.info[uid]["total_last"] += 1
+            self.info[uid]["update_last"] += 1
+
+        reply = hxs.xpath('//ul[@class="topic-reply"]/li')
+        for li in reply:
+            href = li.xpath('div/a/@href')
+            if href and href.extract()[0].split('/')[-2] == self.info[uid]["author"]:
+                self.info[uid]["update_last"] += 1
+            self.info[uid]["total_last"] += 1
+        self.cur.execute('update douban set last=%d, `update`=%d, update_last=%d, `total`=%d, total_last=%d where uid=%s' %
+                (int(page), self.info[uid]["update"], self.info[uid]["update_last"], self.info[uid]["total"], self.info[uid]["total_last"], uid))
+        self.conn.commit()
 
     def parse(self, response):
-        url = response.url.split('=')
-        page = url[1]
-        url = url[0].split('/')[-2]
+        uid = get_id(response.url)
+        page = get_page(response.url)
         hxs = Selector(response)
         next_page = hxs.xpath('//div[@class="paginator"]/span[@class="next"]/a/@href').extract()
 
-        url = os.path.join("douban", url)
-        if not os.path.exists(url):
-            os.mkdir(url)
-        f = open(os.path.join(url, page), 'w')
+        path = os.path.join("/home/ya/Desktop/douban", uid)
+        if not os.path.exists(path):
+            os.mkdir(path)
+        f = open(os.path.join(path, page), 'w')
 
         if page == "0":
             topic = hxs.xpath('//div[@class="topic-content clearfix"]/div[@class="topic-doc"]')
@@ -56,9 +84,17 @@ class douban(BaseSpider):
             f.write('\n')
             f.write(topic.xpath('div[@id="link-report"]/div[@class="topic-content"]').extract()[0].strip())
             f.write('\n')
-        reply = hxs.xpath('//ul[@class="topic-reply"]').extract()
-        f.write(reply[0].strip())
+            self.info[uid]["total"] += 1
+            self.info[uid]["update"] += 1
+        reply = hxs.xpath('//ul[@class="topic-reply"]')
+        f.write(reply.extract()[0].strip())
         f.close()
+
+        for li in reply.xpath('li'):
+            href = li.xpath('div/a/@href')
+            if href and href.extract()[0].split('/')[-2] == self.info[uid]["author"]:
+                self.info[uid]["update"] += 1
+            self.info[uid]["total"] += 1
 
         if next_page:
             return [self.make_requests_from_url(next_page[0])]
--- spy/spiders/tianya.py
+++ spy/spiders/tianya.py
@@ -33,15 +33,19 @@ class tianya(BaseSpider):
     allowed_domains = ["bbs.tianya.cn"]
 
     start_urls = []
-    cur.execute('select url, next from tianya')
+    cur.execute('select uid, last, `update`, update_last, `total`, total_last, author from tianya')
     rsts = cur.fetchall()
     info = {}
     for rst in rsts:
-        uid = get_id(rst[0])
-        page = rst[1]
-        info[uid] = {"next": page}
-        url = "http://bbs.tianya.cn/"+uid+"-"+str(page)+".shtml"
+        url = "http://bbs.tianya.cn/"+rst[0]+"-"+str(rst[1])+".shtml"
         start_urls.append(url)
+
+        uid = get_id(url)
+        info[uid] = {"total": rst[4]-rst[5]}
+        info[uid]["total_last"] = 0
+        info[uid]["update"] = rst[2]-rst[3]
+        info[uid]["update_last"] = 0
+        info[uid]["author"] = rst[6]
 
     def __init__(self, url=None, *args, **kwargs):
         if url:
@@ -52,34 +56,51 @@ class tianya(BaseSpider):
         super(tianya, self).__init__(*args, **kwargs)
 
     def handle_last(self, response):
-        return []
+        uid = get_id(response.url)
+        page = get_page(response.url)
+        hxs = Selector(response)
+        items = hxs.xpath('//div[@class="atl-item"]')
+        for item in items:
+            head = item.xpath('div[@class="atl-head"]/div[@class="atl-info"]')
+            if not head:
+                head = hxs.xpath('//div[@id="post_head"]/div[contains(@class, "atl-menu")]/div[@class="atl-info"]')
+            if head.xpath('span/a/@href').extract()[0].split('/')[-1] == self.info[uid]["author"]:
+                self.info[uid]["update_last"] += 1
+            self.info[uid]["total_last"] += 1
+        self.cur.execute('update tianya set last=%d, `update`=%d, update_last=%d, `total`=%d, total_last=%d where uid="%s"' %
+                (int(page), self.info[uid]["update"], self.info[uid]["update_last"], self.info[uid]["total"], self.info[uid]["total_last"], uid))
+        self.conn.commit()
 
     def parse(self, response):
         uid = get_id(response.url)
         page = int(get_page(response.url))
 
-        url = os.path.join("tianya", uid)
-        if not os.path.exists(url):
-            os.mkdir(url)
-        f = open(os.path.join(url, str(page)), 'w')
+        path = os.path.join("/home/ya/Desktop/tianya", uid)
+        if not os.path.exists(path):
+            os.mkdir(path)
+        f = open(os.path.join(path, str(page)), 'w')
 
         hxs = Selector(response)
         items = hxs.xpath('//div[@class="atl-item"]')
         for item in items:
-            head = item.xpath('div[@class="atl-head"]/div[@class="atl-info"]').extract()
-            body = item.xpath('div[@class="atl-content"]/div[contains(@class, "atl-con-bd")]/div[contains(@class, "bbs-content")]').extract()
+            head = item.xpath('div[@class="atl-head"]/div[@class="atl-info"]')
+            body = item.xpath('div[@class="atl-content"]/div[contains(@class, "atl-con-bd")]/div[contains(@class, "bbs-content")]')
             if head:
-                f.write(head[0].strip())
+                f.write(head.extract()[0].strip())
             else:
-                head = hxs.xpath('//div[@id="post_head"]/div[contains(@class, "atl-menu")]/div[@class="atl-info"]').extract()
-                f.write(head[0].strip())
+                head = hxs.xpath('//div[@id="post_head"]/div[contains(@class, "atl-menu")]/div[@class="atl-info"]')
+                f.write(head.extract()[0].strip())
             f.write('\n')
-            f.write(body[0].strip())
+            f.write(body.extract()[0].strip())
             f.write("\n\n")
+            if head.xpath('span/a/@href').extract()[0].split('/')[-1] == self.info[uid]["author"]:
+                self.info[uid]["update"] += 1
+            self.info[uid]["total"] += 1
         f.close()
 
         next_page = hxs.xpath('//div[@class="atl-pages"]/form/a[@class="js-keyboard-next"]/@href').extract()
         if next_page:
             return [self.make_requests_from_url("http://bbs.tianya.cn"+next_page[0])]
         else:
+            self.handle_last(response)
             return []
